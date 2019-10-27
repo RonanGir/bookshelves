348e588e464424616f8aba79331157a08947cd2d
Ronan Girault <girault.ronan@gmail.com>
2019-10-27 14:16:03 +0100

    init app structure + auth system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyCg274AFFf6izRv94oKJBq2TIaAZkD4AHk",
+      authDomain: "bookshelves-1cbd2.firebaseapp.com",
+      databaseURL: "https://bookshelves-1cbd2.firebaseio.com",
+      projectId: "bookshelves-1cbd2",
+      storageBucket: "bookshelves-1cbd2.appspot.com",
+      messagingSenderId: "726122688363",
+      appId: "1:726122688363:web:b6f21454bda0d352f7662e"
+    }
+    firebase.initializeApp(config);
+  }
+
 }
---
+++ src/app/models/book.model.ts
@@ -0,0 +1,9 @@
+export class Model {
+
+  constructor(public title: string,
+              public author: string,
+              public photo?: string,
+              public synopsis?: string
+              ) {}
+
+}
